449d357c25e85bdec8f3273948a929593ac49933
Jorge Carballo <jelcaf@gmail.com>
2017-02-14 14:02:36 +0000

    Fix some eslint errors

--- backend/memory.js
+++ backend/memory.js
@@ -4,24 +4,25 @@ var debug = require('debug')('loopback:ibmresearch:mixins:backend:memory');
 // Wrapper for memory-cache
 function MemoryWrapper() {
   return {
-    setex: function (key, ttl, value) {
+    setex(key, ttl, value) {
       cache.put(key, JSON.stringify(value), ttl);
     },
-    ttl: function (key, cb) {
+    ttl(key, cb) {
       var cachedResults = cache.get(key);
       cb(null, cachedResults ? 1 : undefined);
     },
-    del: function (key) {
+    del(key) {
       cache.del(key);
     },
-    keys: function (keyExpression, cb) {
+    keys(keyExpression, cb) {
       var re = new RegExp(keyExpression);
       var allKeys = cache.keys();
       var filteredKeys = allKeys.filter(function isModelKey(value) {
         return value && re.test(value);
       });
+      cb(null, filteredKeys);
     },
-    get: function (key, cb) {
+    get(key, cb) {
       var cachedResults = cache.get(key);
       cb(null, JSON.parse(cachedResults));
     }
@@ -30,11 +31,11 @@ function MemoryWrapper() {
 
 
 function Client(config) {
-  debug('Configure memory cache');
+  debug('Configure memory cache: ', config);
   this.client = MemoryWrapper();
 }
 
-Client.prototype.getClient = function() {
+Client.prototype.getClient = function () {
   return this.client;
 };
 
--- backend/redis.js
+++ backend/redis.js
@@ -19,7 +19,7 @@ function getUrl(config) {
     return undefined;
   }
 
-  var url = 'redis://x:'+config.password+'@'+config.host+':'+config.port+"/"+config.dbNumber;
+  const url = `redis://x:${config.password}@${config.host}:${config.port}/${config.dbNumber}`;
   debug('Url redis backend: %s', url);
   return url;
 }
@@ -30,23 +30,23 @@ function Client(redisConfig) {
     return undefined;
   }
 
-  this.client = redisModule.createClient({ url: url });
+  this.client = redisModule.createClient({ url });
 
-  this.client.on("ready", function (val) {
-    debug("Ready " + val);
+  this.client.on('ready', function (val) {
+    debug(`Ready ${val}`);
   });
 
-  this.client.on("error", function (err) {
-      debug("Error " + err);
-      console.error(err);
+  this.client.on('error', function (err) {
+    debug(`Error ${err}`);
+    console.error(err);
   });
 
-  this.client.on("reconnecting", function (err) {
-      debug("Reconnecting " + err);
+  this.client.on('reconnecting', function (err) {
+    debug(`Reconnecting ${err}`);
   });
 }
 
-Client.prototype.getClient = function() {
+Client.prototype.getClient = function () {
   return this.client;
 };
 
