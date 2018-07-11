5c6c0b87abf816df1f281e437bad9bfd57d3995a
Jorge Carballo <jelcaf@gmail.com>
2018-07-11 11:50:03 +0100

    Adjust model-cache to work with methods with multiple params

--- test/test-app/common/models/modelcache-item.js
+++ test/test-app/common/models/modelcache-item.js
@@ -33,4 +33,23 @@ module.exports = function (Model) {
       returns: [{ arg: 'items', root: true, type: ['object'] }]
     }
   );
+
+  ModelcacheItem.methodWith2Param = function (parameter1, parameter2, cb) {
+    setTimeout(function () {
+      ModelcacheItem.find(function (err, models) {
+        return cb(err, models);
+      });
+    }, 100);
+  };
+
+  ModelcacheItem.remoteMethod(
+    'methodWith2Param', {
+      accepts: [
+        { arg: 'parameter1', type: 'string' },
+        { arg: 'parameter2', type: 'string' },
+      ],
+      http: { path: '/methodWith2Param', verb: 'get', errorStatus: 400 },
+      returns: [{ arg: 'items', root: true, type: ['object'] }]
+    }
+  );
 };
